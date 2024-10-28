f7df5692c2a9233a96ae37f9784b575325fdfbbc
sanpitka <sanpitka@student.oulu.fi>
2024-10-28 17:30:44 +0200

    5 tests implemented and passed

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -30,7 +30,6 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(8, 1))
         f = Frame(2,6)
         game.add_frame(f)
-
         self.assertEqual(f, game.get_frame_at(9))
 
     def test_game_created_11_frames(self):
@@ -47,4 +46,20 @@ class TestBowlingGame(unittest.TestCase):
         f = Frame(2,6)
         game.add_frame(f)
         self.assertRaises(BowlingError, game.add_frame, Frame(1,1))
+
+    def test_calculate_score(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        f = Frame(2, 6)
+        game.add_frame(f)
+        self.assertEqual(81, game.calculate_score())
+
 
