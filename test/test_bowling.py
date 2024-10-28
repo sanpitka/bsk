d3150bd2e99dd8e0d24a278168873f2454e086ae
sanpitka <sanpitka@student.oulu.fi>
2024-10-28 17:52:59 +0200

    User Story 5 tests implemented and passed

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -91,3 +91,18 @@ class TestBowlingGame(unittest.TestCase):
         f = Frame(2, 6)
         game.add_frame(f)
         self.assertEqual(94, game.calculate_score())
+
+    def test_calculate_score_strike_spare(self):
+        game = BowlingGame()
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(4, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        f = Frame(2, 6)
+        game.add_frame(f)
+        self.assertEqual(103, game.calculate_score())
