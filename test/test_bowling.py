265ef7b6c1a4dc9976c52c697d76559e8d366219
sanpitka <sanpitka@student.oulu.fi>
2024-10-28 17:00:51 +0200

    2 tests implemented and passed

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -8,4 +8,12 @@ from frame import Frame
 class TestBowlingGame(unittest.TestCase):
 
     def test_something(self):
-        pass
+        f = Frame(5,2)
+        game = BowlingGame()
+        game.add_frame(f)
+        self.assertEqual(f, game.get_frame_at(0))
+
+    def test_empty_game(self):
+        game = BowlingGame()
+        self.assertRaises(BowlingError, game.get_frame_at, 0)
+
