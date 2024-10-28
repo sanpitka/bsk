9b034fe11715d53cebed14a68cb384e2f098ecfd
sanpitka <sanpitka@student.oulu.fi>
2024-10-28 17:22:47 +0200

    testing the token

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -30,6 +30,7 @@ class TestBowlingGame(unittest.TestCase):
         game.add_frame(Frame(8, 1))
         f = Frame(2,6)
         game.add_frame(f)
+
         self.assertEqual(f, game.get_frame_at(9))
 
     def test_game_created_11_frames(self):
